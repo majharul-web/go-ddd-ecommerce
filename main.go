04c047ad65b1b929f6cf3ad03113768c1a3359f0
majharul <majharuljony163@gmail.com>
2025-09-29 15:56:08 +0600

    create basic server

--- main.go
+++ main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, World!")
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/hello", helloHandler)
+
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}else{
+		fmt.Println("Server started on :8080")
+	}
 }
