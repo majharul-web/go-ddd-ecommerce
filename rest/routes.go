8d7396c0da0fc63f3359d8a749dbc144b5ae4e79
majharul-web <majharuljony163@gmail.com>
2025-10-05 19:49:54 +0600

    add user module

--- rest/routes.go
+++ rest/routes.go
@@ -7,12 +7,22 @@ import (
 )
 
 func initRoutes(mux *http.ServeMux, manager *middleware.Manager) {
+	// ✅ Test route
 	mux.Handle("GET /test",
 		manager.With(http.HandlerFunc(handlers.Test)))
 
+	// ✅ Product routes
 	mux.Handle("GET /products", manager.With(http.HandlerFunc(handlers.GetProductList), middleware.MoreMiddleware))
 	mux.Handle("POST /products", manager.With(http.HandlerFunc(handlers.CreateProduct)))
 	mux.Handle("GET /products/{productId}", manager.With(http.HandlerFunc(handlers.GetProductByID)))
 	mux.Handle("PUT /products/{productId}", manager.With(http.HandlerFunc(handlers.UpdateProduct)))
 	mux.Handle("DELETE /products/{productId}", manager.With(http.HandlerFunc(handlers.DeleteProduct)))
+
+	// ✅ User routes
+	mux.Handle("GET /users", manager.With(http.HandlerFunc(handlers.GetUserList), middleware.MoreMiddleware))
+	mux.Handle("POST /users", manager.With(http.HandlerFunc(handlers.CreateUser)))
+	mux.Handle("GET /users/{userId}", manager.With(http.HandlerFunc(handlers.GetUserByID)))
+	mux.Handle("PUT /users/{userId}", manager.With(http.HandlerFunc(handlers.UpdateUser)))
+	mux.Handle("DELETE /users/{userId}", manager.With(http.HandlerFunc(handlers.DeleteUser)))
+	mux.Handle("POST /users/login", manager.With(http.HandlerFunc(handlers.LoginUser)))
 }
